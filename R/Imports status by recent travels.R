e5134833ac5b0a70b55d478c9ee8c3f0ffb5b02d
Su Myat Han <63596173+Su06690@users.noreply.github.com>
2021-02-11 17:48:10 +0900

    Add files via upload

--- R/Imports status by recent travels.R
+++ R/Imports status by recent travels.R
@@ -1,7 +1,6 @@
 rm(list = ls())
 
 library(o2geosocial)
-library(data.table)
 
 load ("data/kamigoto_combined_data.RData")
 
@@ -9,17 +8,20 @@ load ("data/kamigoto_combined_data.RData")
 # Distribution of the latent/incubation period  average 1.63 ± 0.06 days and standard deviation (sd) 0.26 ± 0.08 days A.cori etal (https://doi.org/10.1016/j.epidem.2012.06.001)
 # 2 days (1-4 days range)
 f_dens <- dgamma(x = 1:100, scale = 0.04147239, shape = 39.30325)
+#f_dens<- f_dens/sum(f_dens)
 
 #?distribution of infectious period average 0.99 ± 0.25 days and sd 0.96 ± 0.15 days , A.cori etal (https://doi.org/10.1016/j.epidem.2012.06.001)
+
+
 # Distribution of the generation time 10.1371/journal.pone.0075339  mean 3.2 (2.4-3.9)
-
 #SD is calculated  from the formula from cochrone http://handbook-5-1.cochrane.org/chapter_7/7_7_3_2_obtaining_standard_deviations_from_standard_errors_and.htm 
-w_dens <- dnorm(x = 1:100, mean = 3.2, sd = 4.0)
-
+w_dens <- dnorm(x = 1:100, mean = 3.2, sd = 0.4)
+# a mean serial interval of 3.6 days (95% confidence interval = 2.9–4.3 days), with standard deviation 1.6 days
 
 ## ----age, message = FALSE, warning = FALSE-----------------------------------------------------------------------
 # either jpoly2 created or new social contacft study of nishiura etal
 library(readxl)
+#currently using weekday socail contact
 social_contact<- read_excel("data/contact_weekday.xlsx", 
                             col_types = c("numeric", "numeric", "numeric", 
                                           "numeric", "numeric", "numeric", 
@@ -95,7 +97,10 @@ config1 <- create_config(data = data1,
                          n_iter_import = 10000, #Iteration number: short run
                          burnin = 5000, #burnin period: first run
                          outlier_relative = T, #Absolute / relative threshold 
-                         outlier_threshold = 0.9 #Value of the threshold
+                         outlier_threshold = 0.9, #Value of the threshold
+                         prior_a = c(.2, 5),
+                         delta=8, #tmeporal threshold for the pre-clsutering , short 
+                         verbatim =TRUE
 )
 
 # Run model 1:no inference of the importation status of cases,
@@ -119,10 +124,44 @@ config2 <- create_config(data = data2,
                          find_import = FALSE, # No inference of import status
                          n_iter = 20000, 
                          sample_every = 50, # 1 in 50 iterations is kept
-                         burnin = 5000)
+                         prior_a = c(.2, 5),
+                         burnin = 5000,  
+                         delta=8, #tmeporal threshold for the pre-clsutering , short 
+                         verbatim =TRUE
+)
+
 # Run model 2
 out2 <- outbreaker(data = data2, config = config2, moves = moves, 
                    priors = priors, likelihoods = likelihoods)
+
+
+# Set data and config for model 3
+data3 <- outbreaker_data(dates = finalData$visit_date, 
+                         age_group = finalData$age_gp,
+                         region = finalData$district,
+                         #genotype = dt_cases$Genotype, 
+                         w_dens = w_dens, 
+                         f_dens = f_dens, 
+                         a_dens = a_dens,
+                         population = pop_vect, 
+                         distance = dist_mat,
+                         import = finalData$import #Import status of the cases
+)
+
+config3 <- create_config(data = data3, 
+                         find_import = TRUE, #inference of import status
+                         n_iter = 20000, 
+                         sample_every = 50, # 1 in 50 iterations is kept
+                         burnin = 5000,  
+                         prior_a = c(.2, 5),
+                         delta=8, #tmeporal threshold for the pre-clsutering , short 
+                         verbatim =TRUE
+)
+
+# Run model 3
+out3 <- outbreaker(data = data3, config = config3, moves = moves, 
+                   priors = priors, likelihoods = likelihoods)
+
 
 ## ----params, warning = FALSE-------------------------------------------------------------------------------------
 # Summary parameters a and b, removing the burnin-period
@@ -132,102 +171,43 @@ print(summary(out1, burnin = 5000)$b)
 # Model 2
 print(summary(out2, burnin = 5000)$a)
 print(summary(out2, burnin = 5000)$b)
-
+# Model 2¥3
+print(summary(out3, burnin = 5000)$a)
+print(summary(out3, burnin = 5000)$b)
 
 ## ----prepare_plot, warning = FALSE-------------------------------------------------------------------------------
-# I have modified the breakdown of the cluster size for visualization purpose
-group_cluster <- c(1,2, 5,50, 100, 1000) - 1
+# We create groups of cluster size: initialise the breaks for each group
+
+##to make the smaller clusters size 
+group_cluster <- c(1,2,5,10,50,80,100) - 1
+
 
 # Grouped cluster size distribution in each run
 clust_infer1 <- summary(out1, group_cluster = group_cluster, 
                         burnin = 5000)$cluster
 clust_infer2 <- summary(out2, group_cluster = group_cluster, 
                         burnin = 5000)$cluster
+clust_infer3 <- summary(out3, group_cluster = group_cluster, 
+                        burnin = 5000)$cluster
 # Merge inferred and reference cluster size distributions into one matrix
-clust_size_matrix <- rbind(clust_infer1["Median",], clust_infer2["Median",])
+clust_size_matrix <- rbind(clust_infer1["Median",], clust_infer2["Median",],clust_infer3["Median",])
 
 
 ## ----plot_first, warning = FALSE, fig.width = 6.5, fig.height = 4, fig.cap= "Figure 3: Comparison of inferred cluster size distribution with the reference data"----
 # Histogram of the inferred and reference cluster size distributions 
 b <- barplot(clust_size_matrix, names.arg = colnames(clust_infer1), las=1,
              ylab = "Number of clusters", xlab = "Cluster size", main = "", 
-             beside = T, ylim = c(0, max(c(clust_infer1, clust_infer2))))
+             beside = T, ylim = c(0, max(c(clust_infer1, clust_infer2, clust_infer3))))
 # Add the 50% CI
 arrows(b[1,], clust_infer1["1st Qu.",], b[1,], clust_infer1["3rd Qu.",], 
        angle = 90, code = 3, length = 0.1)
 arrows(b[2,], clust_infer2["1st Qu.",], b[2,], clust_infer2["3rd Qu.",], 
        angle = 90, code = 3, length = 0.1)
+arrows(b[3,], clust_infer3["1st Qu.",], b[3,], clust_infer3["3rd Qu.",], 
+       angle = 90, code = 3, length = 0.1)
 # Add legend
 legend("topright", fill = grey.colors(2), bty = "n",
-       legend = c("Inferred import status",  "Simulated dataset"))
-
----------------#####My dataset has no information on infector ID and cluster number or generation ######------------------------
-## ----index_infer, warning = FALSE--------------------------------------------------------------------------------
-#' Title: Compute the proportion of iterations in the outbreaker() output 
-#` where the inferred index matches the actual index in dt_cases
-#'
-#' @param finalData: reference dataset
-#' @param out: Matrix output of outbreaker()
-#' @param burnin: Numeric, length of the burnin phase
-#'
-#' @return Numeric vector showing the proportion of iterations pointing to
-#' the correct index case
-#index_infer <- function(finalData, out, burnin){
-  ## Generate the data frame listing every infector:
-  # Select rows above burnin, and columns describing who infected whom
-  # out_index <- out[out$step > burnin, grep("alpha", colnames(out))]
-  # ID of each infector
-  #ID_index <- matrix(finalData[unlist(out_index), ID], ncol = nrow(dt_cases))
-  # Match inferred (ID_index) and actual infector (column infector_ID)
- # match_infer_data <- t(ID_index) == dt_cases$infector_ID
-  # If a case is rightly inferred as an ancestor, set match to TRUE
-  #match_infer_data[is.na(t(ID_index)) & is.na(dt_cases$infector_ID)] <- TRUE
-  #prop_correct <- rowSums(match_infer_data, na.rm = T)/ncol(match_infer_data)
-  
-  return(prop_correct)
-}
-# Same as index_infer, except it returns the proportion of inferred indexes
-# who are in the same reference cluster as the case
-#index_clust <- function(dt_cases, out, burnin){
-  ## Generate the data frame listing every infector:
-  # Select rows above burnin, and columns describing who infected whom
- # out_index <- out[out$step > burnin, grep("alpha", colnames(out))]
-  # cluster of each infector
-  #clust_index <- matrix(dt_cases[unlist(out_index), cluster], 
-                        ncol = nrow(dt_cases))
-  # Match inferred (cluster_index) and actual cluster (column cluster)
-  #match_infer_data <- t(clust_index) == dt_cases$cluster
-  # Exclude ancestors
-  #match_infer_data <- match_infer_data[!is.na(dt_cases$infector_ID),]
-  
-  
-  #prop_correct <- rowSums(match_infer_data, na.rm = T)/ncol(match_infer_data)
-  
-  #return(prop_correct)
-#}
-# Run index_infer for each model
-#index_infer1 <- index_infer(dt_cases = dt_cases, out = out1, burnin = 5000)
-#index_infer2 <- index_infer(dt_cases = dt_cases, out = out2, burnin = 5000)
-# Run index_clust for each model
-#index_clust1 <- index_clust(dt_cases = dt_cases, out = out1, burnin = 5000)
-#index_clust2 <- index_clust(dt_cases = dt_cases, out = out2, burnin = 5000)
-
-
-## ----plot index_infer, warning = FALSE, fig.width = 7, fig.height = 3, fig.cap = "Figure 4: Panel A: Proportion of iterations with the correct index for each case; Panel B: Proportion of iterations where the index is from the correct cluster"----
-# Plot the sorted proportion in each model
-#par(bty = "n", mfrow = c(1, 2), mar = c(5,4,2,0), oma = c(0, 0, 0, 0))
-# Panel A: Perfect match
-#plot(sort(index_infer1), type = "l", ylab = "Proportion of iterations", xlab = "Case", 
- #    main =  "A", las=1, col = grey.colors(3)[1], lwd = 3)
-#lines(sort(index_infer2), col = grey.colors(3)[2], lwd = 3)
-
-# Panel B: Close match
-#plot(sort(index_clust1), type = "l", xlab = "Case", ylab = "", 
- #    main =  "B", las=1, col = grey.colors(3)[1], lwd = 3)
-#lines(sort(index_clust2), col = grey.colors(3)[2], lwd = 3)
-#legend("bottomright", col = grey.colors(3)[1:2], lwd = 3, bty = "n",
- #      legend = c("Inferred import status","Known import status"))
-
+       legend = c("Inferred import status",  "Epi import", "Epi import and inferred import"))
 
 ## ----import_sf_file, message = FALSE, warning = FALSE, fig.width = 6.5, fig.height = 2.5-------------------------
 library(ggplot2)
@@ -268,9 +248,6 @@ map2$prop_reg <- prop_reg2[as.character(map2$S_NAME)]
 # Merge maps
 maps <- rbind(map1, map2)
 
-# Crop map to area of interest
-lim_lon <- c(-84, -82)
-lim_lat <- c(40, 41.5)
 maps <- maps[maps$X_CODE > lim_lon[1] & maps$X_CODE < lim_lon[2] & 
                maps$Y_CODE > lim_lat[1] & maps$Y_CODE< lim_lat[2],]
 
@@ -323,14 +300,10 @@ map2$n_sec <- as.numeric(n_sec2[as.character(map2$S_NAME)])
 ## ----create_maps2, warning = FALSE, fig.width = 6.5, fig.height = 2.5, fig.cap = "Figure 6: Median number of secondary transmission per case in each census tract"----
 # Merge maps
 maps_n_sec <- rbind(map1, map2)
-# Crop map to area of interest
-#lim_lon <- c(-84, -82)
-#lim_lat <- c(40, 41.5)
-#maps_n_sec <- maps_n_sec[maps_n_sec$X_CODE > lim_lon[1] &
+maps_n_sec <- maps_n_sec[maps_n_sec$INTPTLON > lim_lon[1] &
                            maps_n_sec$INTPTLON < lim_lon[2] &
                            maps_n_sec$INTPTLAT > lim_lat[1] & 
                            maps_n_sec$INTPTLAT < lim_lat[2],]
-
 # Plot the geographical distribution of the number of secondary cases
 ggplot(maps_n_sec) +  geom_sf(aes(fill = n_sec)) + facet_grid(~model)  +     
   scale_fill_gradient2(na.value = "lightgrey", mid = "lightblue",
@@ -338,176 +311,3 @@ ggplot(maps_n_sec) +  geom_sf(aes(fill = n_sec)) + facet_grid(~model)  +
                        breaks = seq(0, 5, 0.5),name = "Sec cases") +
   #coord_sf(xlim = c(-83.8, -82.2), ylim = c(40.2, 41.3)) +
   theme_classic(base_size = 9) 
-
-## ----create_stouff_matrix, message = FALSE, warning = FALSE------------------------------------------------------
-# For every column of the distance matrix, use the cumulative sum of the 
-# population vector ordered by the distance. Remove the values where 
-# the distance between the regions is above gamma
-dist_mat_stouffer <- apply(dist_mat, 2, function(X){
-  pop_X <- cumsum(pop_vect[order(X)])
-  omega_X <- pop_X[names(X)]
-  # omega_X is set to -1 if the distance between two regions is above gamma
-  omega_X[X > config1$gamma] <- -1
-  return(omega_X)
-})
-# The new value of gamma is equal to the maximum of dist_mat_stouffer + 1
-gamma <- max(dist_mat_stouffer) + 1
-# The values previously set to -1 are now set to the new value of gamma
-dist_mat_stouffer[dist_mat_stouffer == -1] <- max(dist_mat_stouffer) * 2
-
-
-## // [[Rcpp::depends(o2geosocial)]]
-
-## #include <Rcpp.h>
-
-## #include <Rmath.h>
-
-## #include <o2geosocial.h>
-
-## // This function is used to estimate new values of the spatial parameter.
-
-## // It is based on the structure as cpp_move_a in o2geosocial,
-
-## // [[Rcpp::export()]]
-
-## Rcpp::List cpp_stouffer(Rcpp::List param, Rcpp::List data, Rcpp::List config,
-
-##                         Rcpp::RObject custom_ll, Rcpp::RObject custom_prior){
-
-##   // Import parameters
-
-##   Rcpp::List new_param = clone(param);
-
-##   double gamma = config["gamma"];
-
-##   int max_kappa = config["max_kappa"];
-
-##   Rcpp::List new_log_s_dens = new_param["log_s_dens"];
-
-##   Rcpp::NumericMatrix dist = data["distance"], probs = new_log_s_dens[0];
-
-##   Rcpp::NumericMatrix ances = data["can_be_ances_reg"];
-
-##   Rcpp::NumericVector pop = data["population"], limits = config["prior_a"];
-
-##   // Size of the probability matrix
-
-##   int nb_cases = pow(probs.size(), 0.5);
-
-##   // Draw new value of a
-
-##   Rcpp::NumericVector new_a = new_param["a"];
-
-##   double sd_a = static_cast<double>(config["sd_a"]);
-
-##   double old_logpost = 0.0, new_logpost = 0.0, p_accept = 0.0;
-
-##   // proposal (normal distribution with SD: config$sd_a)
-
-##   new_a[0] += R::rnorm(0.0, sd_a); // new proposed value
-
-##   if (new_a[0] < limits[0] || new_a[0] > limits[1]) return param;
-
-##   // Generate new probability matrix
-
-##   new_param["log_s_dens"] =
-
-##     o2geosocial::cpp_log_like(pop, dist, ances, new_a[0], new_a[0],
-
-##                               max_kappa, gamma, "power-law", nb_cases);
-
-##   // Compare old and new likelihood values
-
-##   old_logpost = o2geosocial::cpp_ll_space(data, config, param,
-
-##                                           R_NilValue, custom_ll);
-
-##   new_logpost = o2geosocial::cpp_ll_space(data, config, new_param,
-
-##                                           R_NilValue, custom_ll);
-
-##   // Add prior values
-
-##   old_logpost += o2geosocial::cpp_prior_a(param, config, custom_prior);
-
-##   new_logpost += o2geosocial::cpp_prior_a(new_param, config, custom_prior);
-
-##   // Accept or reject proposal
-
-##   p_accept = exp(new_logpost - old_logpost);
-
-##   if (p_accept < unif_rand()) return param;
-
-##   return new_param;
-
-## }
-
-##### I didnt try anything from here
-## ----moves_priors_data_conf, message = FALSE, warning = FALSE----------------------------------------------------
-# Edit the lists of movements and priors
-moves3 <- custom_moves(a = cpp_stouffer)
-# Define null function
-f_null <- function(param) {
-  return(0.0)
-}
-priors3 <- custom_priors(b = f_null)
-
-
-# Set data and config lists
-data3 <- outbreaker_data(dates = dt_cases$Date, #Onset dates
-                         age_group = dt_cases$age_group, #Age group
-                         region = dt_cases$Cens_tract, #Location
-                         genotype = dt_cases$Genotype, #Genotype
-                         w_dens = w_dens, #Serial interval
-                         f_dens = f_dens, #Latent period
-                         a_dens = a_dens, #Age stratified contact matrix
-                         population = pop_vect, #Population 
-                         distance = dist_mat_stouffer #Distance matrix
-)
-config3 <- create_config(data = data3, 
-                         gamma = gamma,
-                         init_b = 0, move_b = FALSE, # b is not estimated
-                         n_iter = 20000, #Iteration number: main run
-                         n_iter_import = 10000, #Iteration number: short run
-                         burnin = 5000, #burnin period: first run
-                         outlier_relative = T, #Absolute / relative threshold
-                         outlier_threshold = 0.9 #Value of the threshold
-)
-# Run the model using the Stouffer's rank method
-out_stouffer <- outbreaker(data = data3, config = config3, moves = moves3, 
-                           priors = priors3, likelihoods = likelihoods)
-
-
-## ----stouffer_cluster, message = FALSE, warning = FALSE, fig.width = 6.5, fig.height = 4, fig.cap = "Figure 7: Comparison of inferred cluster size distribution with the reference data"----
-# Grouped cluster size distribution in the Stouffer's rank model
-clust_infer_stouf <- summary(out_stouffer, burnin = 5000, 
-                             group_cluster = group_cluster)$cluster
-# Merge inferred and reference cluster size distributions
-clust_size_matrix <- rbind(clust_infer_stouf["Median",], h$counts) 
-# Plot the two distributions
-b <- barplot(clust_size_matrix, names.arg = colnames(clust_infer_stouf), 
-             beside = T, ylab = "Number of clusters", xlab = "Cluster size", 
-             main = "", las = 1)
-# Add CIs
-arrows(b[1,], clust_infer_stouf["1st Qu.",], b[1,], 
-       clust_infer_stouf["3rd Qu.",], angle = 90, code = 3, length = 0.1)
-legend("topright", fill = grey.colors(2), bty = "n",
-       legend = c("Inferred import status, Stouffer's rank method", 
-                  "Simulated dataset"))
-
-
-## ----match_stouffer, message = FALSE, warning = FALSE, fig.width = 7, fig.height = 3, fig.cap = "Figure 8: Panel A: Proportion of iterations with the correct index for each case; Panel B: Proportion of iterations where the index is from the correct cluster"----
-# Generate the proportion of perfect and close match for each case in out3
-index_infer_stouf <- index_infer(dt_cases = dt_cases, out = out_stouffer, 
-                                 burnin = 5000)
-index_clust_stouf <- index_clust(dt_cases = dt_cases, out = out_stouffer, 
-                                 burnin = 5000)
-# Plot the sorted proportion in each model
-par(bty = "n", mfrow = c(1, 2), mar = c(5,4,2,0), oma = c(0, 0, 0, 0))
-# Panel A: Perfect match
-plot(sort(index_infer_stouf), main = "A", col = grey.colors(2)[1], lwd = 3,
-     xlab = "Case", ylab = "Proportion of iterations", type = "l", las=1)
-# Panel B: Close match
-plot(sort(index_clust_stouf), type = "l", ylab = "", xlab = "Case", 
-     main =  "B", las=1, col = grey.colors(2)[1], lwd = 3)
-
